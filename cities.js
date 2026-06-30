637de3be3f74dd79132410ef21a4b3656ae0da4e
Sreehari Annam <sannam@wescodist.com>
2026-06-29 21:22:40 -0400

    Add first 80 OH cities from cities.csv

--- cities.js
+++ cities.js
@@ -1,5 +1,20 @@
 const ohioCities = [
-  // ← paste your city list here
+  "Hinckley", "Chillicothe", "Galloway", "Stow", "Cleveland",
+  "Columbus", "Beachwood", "Clarksville", "Brewster", "Brilliant",
+  "Camden", "Archbold", "Bedford", "Oakwood Village", "Canton",
+  "Brunswick", "Bidwell", "Canal Winchester", "Bellevue", "Bainbridge",
+  "Circleville", "Beallsville", "Cambridge", "Amherst", "Baltimore",
+  "Bradford", "Berkey", "Bridgeport", "Arlington", "Blanchester",
+  "Botkins", "Apple Creek", "Bethesda", "Butler", "Akron",
+  "Carrollton", "Chickasaw", "Beach City", "Canal Fulton", "Campbell",
+  "Athens", "Albany", "Chagrin Falls", "Chesapeake", "Castalia",
+  "Bristolville", "Amelia", "Bluffton", "Byesville", "Berlin Center",
+  "Blacklick", "Centerburg", "Chauncey", "Chippewa Lake", "Bellville",
+  "Arcanum", "Cincinnati", "Olmsted Falls", "Tallmadge", "Caldwell",
+  "Whitehouse", "Dublin", "Kingsville", "Mount Vernon", "Hilliard",
+  "Springboro", "Chesterland", "Westerville", "Willoughby", "Strongsville",
+  "North Bend", "New Philadelphia", "New Concord", "Piqua", "Delaware",
+  "Englewood", "Buckland", "Ansonia", "Bremen", "Arcadia",
 ];
 
 const BATCH_SIZE = 5;
