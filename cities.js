4b6ac4183b21d68bf68fb0e053e6b32c020264f1
Sreehari Annam <sannam@wescodist.com>
2026-06-29 21:21:22 -0400

    Initial OH scraper setup with GitHub Actions matrix workflow

---
+++ cities.js
@@ -0,0 +1,14 @@
+const ohioCities = [
+  // ← paste your city list here
+];
+
+const BATCH_SIZE = 5;
+
+const batches = {};
+for (let i = 0; i < ohioCities.length; i++) {
+  const batchNum = Math.floor(i / BATCH_SIZE) + 1;
+  if (!batches[batchNum]) batches[batchNum] = [];
+  batches[batchNum].push(ohioCities[i]);
+}
+
+module.exports = { batches };
