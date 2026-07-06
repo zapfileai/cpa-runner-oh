dbf9502d2e75f4605120f3ca6943ce9eb0bd5ff1
Sreehari Annam <sannam@wescodist.com>
2026-07-06 16:52:43 -0400

    batch 9: next cities

--- cities.js
+++ cities.js
@@ -1,20 +1,10 @@
 const ohioCities = [
-  "Franklin", "Derwent", "Felicity", "Cygnet", "Forest",
-  "Enon", "Delta", "Collins", "Diamond", "Fayetteville",
-  "De Graff", "Gallipolis", "Fletcher", "Coolville", "Galion",
-  "Delphos", "Goshen", "Dennison", "Gates Mills", "Deshler",
-  "Continental", "Fort Loramie", "Covington", "Dellroy", "East Liberty",
-  "Creston", "Green", "Friendship", "Doylestown", "Croton",
-  "Fleming", "Edgerton", "Farmersville", "Greenford", "Cloverdale",
-  "East Canton", "Gratis", "Grand River", "Columbiana", "Garrettsville",
-  "Fort Recovery", "Fayette", "Eaton", "Corning", "Gratiot",
-  "Conover", "Fairlawn", "Fredericktown", "Gibsonburg", "Crooksville",
-  "Dresden", "Fowler", "Franklin Furnace", "Georgetown", "East Liverpool",
-  "Gnadenhutten", "Commercial Point", "Cumberland", "Germantown", "Flushing",
-  "Groveport", "Greenfield", "Dalton", "Gambier", "Dunkirk",
-  "Coshocton", "Dundee", "Danville", "Glouster", "Hiram",
-  "Lynchburg", "Lucasville", "Magnolia", "Lake Milton", "Hebron",
-  "Lisbon", "Leetonia", "Hannibal", "Madison", "Homerville",
+  "Hicksville", "Heath", "Harrod", "Houston", "Jackson",
+  "Jeromesville", "Hamler", "Hamersville", "Ironton", "Lewisville",
+  "Hopewell", "Irwin", "Leavittsburg", "Malinta", "Lodi",
+  "Magnetic Springs", "Liberty Center", "Jackson Center", "Kingston", "Lagrange",
+  "Marengo", "Jewett", "Mantua", "Marshallville", "Little Hocking",
+  "Homeworth", "Kelleys Island", "Haskins", "Maria Stein", "Lindsey",
 ];
 
 const BATCH_SIZE = 5;
